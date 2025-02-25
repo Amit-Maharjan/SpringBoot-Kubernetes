d0e2f4fad4f3773abb91ae71bb2d7eea3eacd8d3
Amit Maharjan <maharjanamit8@gmail.com>
2025-02-25 18:42:38 -0500

    Spring Boot project from spring initializr

---
+++ src/test/java/com/maharjan/web/SpringBootKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maharjan.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
